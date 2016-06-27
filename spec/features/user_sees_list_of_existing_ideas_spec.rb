23ce65cb3c0f4fb2eee12a1b5bbf185a514d1642
marinacor1 <marinaspam1234@gmail.com>
2016-06-27 13:36:43 -0600

    Add capybara to testing

--- spec/features/user_sees_list_of_existing_ideas_spec.rb
+++ spec/features/user_sees_list_of_existing_ideas_spec.rb
@@ -2,11 +2,11 @@ require 'rails_helper'
 
 RSpec.feature "visitor sees list of existing ideas" do
   it "shows list of ideas with three qualities" do
-    idea = Idea.create(title: "Pitch idea", body: "Share sales and international potential.", quality: "genius")
+    idea = Idea.create(title: "Pitch idea", body: "Share sales and international potential.", quality: "swill")
     visit root_path
 
     expect(page).to have_content "Pitch idea"
     expect(page).to have_content "Share sales and international potential."
-    expect(page).to have_content "genius"
+    expect(page).to have_content "swill"
   end
 end
