06fafe4c2dbf590dd9de1bc3ca8924b43693cee0
marinacor1 <marinaspam1234@gmail.com>
2016-06-27 13:51:41 -0600

    Add database cleaner

--- Gemfile
+++ Gemfile
@@ -1,6 +1,5 @@
 source 'https://rubygems.org'
 
-
 gem 'rails', '4.2.6'
 gem 'pg', '~> 0.15'
 gem 'sass-rails', '~> 5.0'
@@ -12,6 +11,7 @@ gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
 
 group :development, :test do
+  gem 'database_cleaner'
   gem 'byebug'
   gem 'rspec-rails'
   gem 'launchy'
--- spec/features/user_sees_list_of_existing_ideas_spec.rb
+++ spec/features/user_sees_list_of_existing_ideas_spec.rb
@@ -2,11 +2,11 @@ require 'rails_helper'
 
 RSpec.feature "visitor sees list of existing ideas" do
   it "shows list of ideas with three qualities" do
-    idea = Idea.create(title: "Pitch idea", body: "Share sales and international potential.", quality: "swill")
+    idea = Idea.create(title: "Pitch idea", body: "Share sales.", quality: "swill")
     visit root_path
-
+binding.pry
     expect(page).to have_content "Pitch idea"
-    expect(page).to have_content "Share sales and international potential."
+    expect(page).to have_content "Share sales."
     expect(page).to have_content "swill"
   end
 end
