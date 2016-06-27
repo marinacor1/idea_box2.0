8139efabeb1691046ffc7bb7fcf2ba286a1e224f
marinacor1 <marinaspam1234@gmail.com>
2016-06-27 12:47:25 -0600

    Initial idea show in landing page

--- Gemfile
+++ Gemfile
@@ -6,7 +6,7 @@ gem 'pg', '~> 0.15'
 gem 'sass-rails', '~> 5.0'
 gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.1.0'
-
+gem 'capybara'
 gem 'jquery-rails'
 gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
@@ -15,8 +15,11 @@ group :development, :test do
   gem 'byebug'
   gem 'rspec-rails'
   gem 'launchy'
+  gem 'pry'
 end
 
 group :development do
   gem 'web-console', '~> 2.0'
 end
+
+gem 'rails_12factor', group: :production
---
+++ spec/features/user_sees_list_of_existing_ideas_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.feature "visitor sees list of existing ideas" do
+  it "shows list of ideas with three qualities" do
+    idea = Idea.create(title: "Pitch idea", body: "Share sales and international potential.", quality: "genius")
+    visit root_path
+
+    expect(page).to have_content "Pitch idea"
+    expect(page).to have_content "Share sales and international potential."
+    expect(page).to have_content "genius"
+  end
+end
