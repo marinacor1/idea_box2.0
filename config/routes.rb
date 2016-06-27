b13399e92be7fc723515693581f117c6482781ce
marinacor1 <marinaspam1234@gmail.com>
2016-06-27 14:51:24 -0600

    Api created

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   root "ideas#index"
 
+  namespace :api do
+    namespace :v1 do
+      resources :ideas, defaults: {format: :json} do 
+      end
+    end
+  end
 end
