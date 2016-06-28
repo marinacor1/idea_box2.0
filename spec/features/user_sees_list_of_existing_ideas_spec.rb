b24a543307de5710ba1f756e173c5d17e3fb9c19
marinacor1 <marinaspam1234@gmail.com>
2016-06-28 14:08:01 -0600

    All tests passing though title is the only one that shows

--- spec/features/user_sees_list_of_existing_ideas_spec.rb
+++ spec/features/user_sees_list_of_existing_ideas_spec.rb
@@ -10,10 +10,12 @@ RSpec.feature "visitor sees list of existing ideas" do
     expect(page).to have_content "swill"
   end
 
-  xit "shows ideas in descending order" do
+  it "shows ideas in descending order" do
     idea = Idea.create(title: "x", body: "y", quality: "swill")
     idea2 = Idea.create(title: "a", body: "b", quality: "plausible")
 
+    visit root_path
+save_and_open_page
     expect(page).to have_content "a"
   end
 end
