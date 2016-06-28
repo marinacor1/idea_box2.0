e94402ed469160808ffa2339cfa7544f6a6258af
marinacor1 <marinaspam1234@gmail.com>
2016-06-27 23:06:02 -0600

    Can edit but does not save

--- app/assets/javascripts/editIdea.js
+++ app/assets/javascripts/editIdea.js
@@ -1,6 +1,5 @@
 function editTitle() {
   $("#idea-title-show").on('blur keydown', function(event){
-    event.stopPropagation();
     if (event.type ==="blur" || event.keyCode === 13) {
       editContent(this, {title: $(this).text()})
     }
@@ -9,9 +8,30 @@ function editTitle() {
 
 function editBody(){
   $("#idea-body-show").on('blur keydown', function(event){
-    event.stopPropagation();
     if (event.type === "blur" || event.keyCode === 13) {
       editContent(this, {body: $(this).text()})
+    }
+  })
+}
+
+function editContent(elementHTML, updatedContent){
+  $.ajax({
+    type: "PATCH",
+    url: "/api/v1/ideas" + elementHTML.parentElement.id,
+    data: {
+      user: {
+        title: updatedContent.title,
+        body: updatedContent.body
+        }
+      },
+      dataType: "json",
+      success: function(datum){
+      $('.user-flash').removeClass('hidden')
+      if (datum['response'] === 'successful') {
+        $('.user-flash').text('Successfully updated!')
+      } else {
+        $('.user-flash').text('Update unsuccessful.')
+      }
     }
   })
 }
