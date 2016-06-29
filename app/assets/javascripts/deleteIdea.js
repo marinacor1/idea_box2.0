f0d64e6497f4ef17d6d972a35ef7955ca0ab139d
marinacor1 <marinaspam1234@gmail.com>
2016-06-29 13:12:27 -0600

    Delete button removes idea

--- app/assets/javascripts/deleteIdea.js
+++ app/assets/javascripts/deleteIdea.js
@@ -1,11 +1,12 @@
 function deleteIdea(){
   $('.any-idea').delegate('.delete-button', 'click', function(){
     var ideaId = $(this).closest('#idea').data('idea-id')
+    var ideaDiv = $(this).closest('#idea')
   var call = {
     type: "DELETE",
     url: "api/v1/ideas/" + ideaId,
     success: function(data) {
-      $('.new-idea' + ideaId).remove();
+      $(ideaDiv).remove();
     }
   }
    $.ajax(call)
