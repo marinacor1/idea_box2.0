520a7019cf460968be8899fb243c1666212cdfe8
marinacor1 <marinaspam1234@gmail.com>
2016-06-29 09:59:48 -0600

    Delete works with old code

--- app/assets/javascripts/deleteIdea.js
+++ app/assets/javascripts/deleteIdea.js
@@ -1,14 +1,11 @@
 function deleteIdea(){
   $('.any-idea').delegate('.delete-button', 'click', function(){
-    debugger
     var ideaId = $(this).closest('#idea').data('idea-id')
-    debugger
   var call = {
     type: "DELETE",
     url: "api/v1/ideas/" + ideaId,
     success: function(data) {
-      debugger
-      $('.new-idea-' + ideaId).remove();
+      $('.new-idea' + ideaId).remove();
     }
   }
    $.ajax(call)
