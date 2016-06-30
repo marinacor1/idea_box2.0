0e47aa739cfc8a95c97c1f05fe3bbf22618d2ee0
marinacor1 <marinaspam1234@gmail.com>
2016-06-29 22:52:07 -0600

    Submit button added

--- app/assets/javascripts/searchIdeas.js
+++ app/assets/javascripts/searchIdeas.js
@@ -1,5 +1,6 @@
 function searchIdeas(){
   $(".search-form").keyup(function(event){
+    binding.pry
     var searchParam = $(this).val().toLowerCase();
     var currentIdeas = $('#index').children();
     currentIdeas.removeClass('invisible');
