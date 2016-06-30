5c02eb6450bb49bc469efa78809bbbed7e23356b
marinacor1 <marinaspam1234@gmail.com>
2016-06-29 19:12:54 -0600

    Search function created but not fully functional

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -5,4 +5,5 @@ $(document).ready(function(){
   deleteIdea()
   increaseQuality()
   decreaseQuality()
+  searchIdeas()
 });
---
+++ app/assets/javascripts/searchIdeas.js
@@ -0,0 +1,13 @@
+function searchIdeas(){
+  $(".search-form").keyup(function(event){
+    var searchParam = $(this).val().toLowerCase();
+    var currentIdeas = $('#index').children();
+    currentIdeas.removeClass('invisible');
+
+    var hideIdeas = allExisitingIdeas.filter(function(){
+    var ideaBodyAndText = $(this).find("#idea-title-show, #idea-body-show").text().toLowerCase()
+    return !(ideaBodyAndText.includes(searchParam))
+  })
+  hideIdeas.addClass('invisible');
+  })
+}
